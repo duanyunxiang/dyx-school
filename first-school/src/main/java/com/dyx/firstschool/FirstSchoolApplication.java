003bd0c1512c1c48333995f68a0ee8b4a2af95ab
duanyunxiang <duanyunxiang@tom.com>
2024-06-06 15:23:36 +0800

    init

---
+++ first-school/src/main/java/com/dyx/firstschool/FirstSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.dyx.firstschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSchoolApplication.class, args);
+    }
+
+}
---
+++ first-school/src/test/java/com/dyx/firstschool/FirstSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyx.firstschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
