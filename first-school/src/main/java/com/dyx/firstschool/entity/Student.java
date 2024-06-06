724fb2441ffbbf5c4e2a3d4c651d1314e77e0bdf
duanyunxiang <duanyunxiang@tom.com>
2024-06-06 15:45:01 +0800

    add entity

---
+++ first-school/src/main/java/com/dyx/firstschool/entity/Student.java
@@ -0,0 +1,18 @@
+package com.dyx.firstschool.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+public class Student implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+}
